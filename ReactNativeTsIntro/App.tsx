38c40eb16999019f0bec9ffcd5289d9f0b3488df
huseynmv <huseynmv@yahoo.com>
2023-01-17 11:26:45 +0400

    Flatlist with TypeScript

--- ReactNativeTsIntro/App.tsx
+++ ReactNativeTsIntro/App.tsx
@@ -1,118 +1,82 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  interface Product{
+  id: number,
+  supplierId: number,
+  categoryId: number,
+  quantityPerUnit: string,
+  unitPrice: number,
+  unitsInStock: number,
+  unitsOnOrder: number,
+  reorderLevel: number,
+  discontinued: boolean,
+  name: string,
+  supplier?: Supplier,
+  category? : Category
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+interface Supplier {
+  id: number,
+  companyName: string,
+  contactName: string,
+  contactTitle: string,
+  address?: Address,
+  category? : Category
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+}
+
+interface Address{
+  street: number,
+  city: string,
+  region: string,
+  postalCode: number,
+  country: string,
+  phone: string,
+}
+
+interface Category{
+  id: number,
+  description: string,
+  name: string
+}
+
+
+  const [products, setproducts] = useState<Product[]>([])
+
+  useEffect(() => {
+      fetch('https://northwind.vercel.app/api/products')
+    .then(res => res.json())
+  .then((data:Product[]) => setproducts(data))
+  }, [])
+  
+
+  const renderItem = ({item}:{item:Product}) => {
+    return (
+      <>
+
+      <Text style = {{fontSize: 24}}>{item.name}</Text>
+
+      </>
+    )
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+   
+      
+    <FlatList
+        data={products}
+        renderItem = {renderItem}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+
+    
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+
+export default App
+
+const styles = StyleSheet.create({})
